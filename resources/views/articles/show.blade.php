bad477c41fe59369401d3eabc104c3767befd4d3
joaocarbonell <joaocarbonellpc@gmail.com>
2020-12-04 17:04:06 -0300

    Subindo implementações relacionadas a Seção 5 do laracast

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,8 +9,8 @@
 
                     <div class="title">
                         <h2>{{$article->title}}</h2>
-                        <p><img src="images/banner.jpg" alt="" class="image image-full" /> </p>
-                        {{$article->body}}}
+                        <p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
+                        {{$article->body}}
                     </div>
 
             </div>
--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,22 @@ Route::get('/about', function () {
 
 });
 
-Route::get('articles',  'App\Http\Controllers\ArticlesController@index');
+Route::get('articles',
+    'App\Http\Controllers\ArticlesController@index');
 
-Route::get('articles/{article}',  'App\Http\Controllers\ArticlesController@show');
+Route::post('articles/',
+    'App\Http\Controllers\ArticlesController@store');
 
+Route::get('articles/create',
+    'App\Http\Controllers\ArticlesController@create');
 
+Route::get('articles/{article}',
+    'App\Http\Controllers\ArticlesController@show');
+
+Route::get('articles/{article}/edit',
+    'App\Http\Controllers\ArticlesController@edit');
+
+Route::put('articles/{article}',
+    'App\Http\Controllers\ArticlesController@update');
 
 
