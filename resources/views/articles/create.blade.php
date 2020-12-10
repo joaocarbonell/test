324aad1a59db795f46d640f0b1684baaaf9d57c3
joaocarbonell <joaocarbonellpc@gmail.com>
2020-12-10 11:28:06 -0300

    JET-14 Implementação da Section 7 do Laracast referente ao eloquent

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -47,6 +47,24 @@
                         @enderror
                     </div>
                 </div>
+
+                <div class="field">
+                    <label class="label" for="body">
+                        Tags
+                    </label>
+                    <div class="select is-multiple control">
+                        <select name="tags[]"
+                        multiple>
+                            @foreach($tags as $tag)
+                                <option value="{{$tag->id}}">{{$tag->name}}</option>
+                            @endforeach
+                        </select>
+                        @error('tags')
+                        <p class="help is-danger">{{$message}}</p>
+                        @enderror
+                    </div>
+                </div>
+
                 <div class="control">
                     <button class="button is-link">Confirmar</button>
                 </div>
